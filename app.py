3c5e3f4a88caac79e35848ef42c2dabca6e4c1ba
Anusha <your-github-email@example.com>
2026-07-09 10:12:36 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+class Employee(BaseModel):
+    name: str
+    deptartment: str
+    salary: float
+@app.get("/")
+def home():
+    return{
+        "message": "Employee Management API"
+    }    
+@app.post("/employee")
+def add_employee(employee:Employee):
+    return{
+        "message":"Employee Added",
+        "data" : employee
+    }    
+
+@app.get("/employee/{emp_id}")
+def get_employee(emp_id:int):
+    return{
+        "Employee ID": emp_id
+    }    
+    
+@app.put("/employee/{emp_id}")
+def update_employee(emp_id:int,employee:Employee):
+    return{
+        "message" : "Employee updated",
+        "Emploee ID": emp_id,
+        "data" : employee
+    }    
+@app.delete("/employee/{emp_id}")
+def delete_employee(emp_id:int):
+    return{
+        "message": "Employee Deleted",
+        "Employee ID": emp_id
+    }    
